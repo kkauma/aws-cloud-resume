00355ef025f3566bc0633533ebfaf717ef7ab483
kkauma <kylekauma@gmail.com>
2024-02-19 14:36:46 -0700

    add additional context in anchor tags for errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,15 @@ export default function Header() {
         <p className="location">Scottsdale, Arizona</p>
         <p className="contact-info">651-983-6096 | kylekauma@gmail.com</p>
         <p className="social-media">
-          <a href="https://www.linkedin.com/in/kylekauma/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/kylekauma/"
+            target="_blank"
+            rel="noreferrer"
+          >
             LinkedIn
           </a>{" "}
           <FontAwesomeIcon icon={faLinkedin} className="linkedin-logo" /> |{" "}
-          <a href="https://github.com/kkauma" target="_blank">
+          <a href="https://github.com/kkauma" target="_blank" rel="noreferrer">
             GitHub
           </a>{" "}
           <FontAwesomeIcon icon={faSquareGithub} className="github-logo" />
